ebb116004000e3239ea972bf29b1dcb1be07be2c
wojtek-kowalczyk <wojtekkowalczyk01@gmail.com>
2022-01-12 13:02:12 +0100

    Cleanup, some refactors - nice, working version

--- CA.py
+++ CA.py
@@ -8,10 +8,29 @@ COLS = 50
 PPU = 16  # px per unit
 PRIMARY_COLOR = (255, 255, 255)  # 1 on grid - WHITE
 SECONDART_COLOR = (0, 0, 0)  # 0 on grid - BLACK
-BORDER = 0
-CA_NEIGHBORS = 4
 ITERATIONS = 1
 DELAY = 1
+WHITE = 1
+BLACK = 0
+
+CA_NEIGHBORS = 4  # set this to 4 for "become majority" behaviour
+PERCENT_FILL = 50  # 0-100
+FILL_WITH = WHITE  # intedned behaviour only for BLACK and WHITE (0 and 1)
+BORDER = BLACK  # BLACK -> gives caves | WHITE -> gives islands
+
+
+# modify this to achieve different result.
+def rule(neighboringOnes, centerValue):
+    """
+    This function returns what the current cell must become dependant on it's neighbors
+    """
+    # neighboringZeros = 8 - neighboringOnes
+    if neighboringOnes > CA_NEIGHBORS:
+        return WHITE
+    elif neighboringOnes == CA_NEIGHBORS:
+        return centerValue
+    else:
+        return BLACK
 
 
 def CA(grid):
@@ -23,29 +42,26 @@ def CA(grid):
 
 
 def seeNeighbors(grid, row, col):
-    countOnes = 0
+    countWhites = 0
     for i in range(-1, 2, 1):
         for j in range(-1, 2, 1):
             if i == 0 and j == 0:
                 continue
             if row+i < 0 or row+i >= ROWS or col+j < 0 or col+j >= COLS:
                 return BORDER
-            if grid[row+i][col+j] == 1:
-                countOnes += 1
-    if countOnes >= CA_NEIGHBORS:
-        return 1
-    else:
-        return 0
+            if grid[row+i][col+j] == WHITE:
+                countWhites += 1
+    return rule(countWhites, grid[row][col])
 
 
 def generateGrid():
-    return [[randrange(2) for i in range(COLS)] for j in range(ROWS)]
+    return [[FILL_WITH if PERCENT_FILL > randrange(100) else abs(FILL_WITH-1) for i in range(COLS)] for j in range(ROWS)]
 
 
 def drawGrid(grid, screen):
     for i in range(ROWS):
         for j in range(COLS):
-            color = PRIMARY_COLOR if grid[i][j] == 1 else SECONDART_COLOR
+            color = PRIMARY_COLOR if grid[i][j] == WHITE else SECONDART_COLOR
             pygame.draw.rect(screen, color,
                              pygame.Rect(j*PPU, i*PPU, PPU, PPU))
     pygame.display.flip()
@@ -60,7 +76,6 @@ def printGrid(grid):
 
 if __name__ == "__main__":
     screen = pygame.display.set_mode((COLS*PPU, ROWS*PPU))
-    screen.fill((150, 150, 150))
     pygame.display.set_caption("Cellular Automaton")
     grid = generateGrid()
     drawGrid(grid, screen)
@@ -74,4 +89,3 @@ if __name__ == "__main__":
             if event.type == pygame.KEYDOWN:
                 grid = CA(grid)
                 drawGrid(grid, screen)
-                time.sleep(0.5)
